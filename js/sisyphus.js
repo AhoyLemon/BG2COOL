d71308769c5cb41280869f15220cb3954adffb2e
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-07-01 15:36:08 -0500

    Initial commit

---
+++ js/sisyphus.js
@@ -0,0 +1,6 @@
+//@prepros-prepend partials/_variables.js
+//@prepros-prepend partials/_functions.js
+//@prepros-prepend partials/_store.js
+//@prepros-prepend partials/_phrases.js
+
+//@prepros-append partials/_vue.js
