6b7ad6fa26bfdcce33a7f74ddcdb5b9ba5eb1002
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-07-16 16:37:21 -0500

    Pattern details sidebar.

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -3,12 +3,16 @@ var app = new Vue({
   data: {
     phase: 'begin',
     activeTitle: 'none',
-    bgPattern: ''
+    bgPattern: '',
+    currentPattern: null
   },
 
   methods: {
     setPattern(pattern) {
       let self = this;
+
+      self.currentPattern = pattern;
+
       self.activeTitle = pattern.title;
       self.bgPattern = pattern.file;
       console.log(pattern.title);
@@ -17,9 +21,11 @@ var app = new Vue({
   },
 
   computed: {
+    /*
     rockLeft() {
       return 'calc('+this.s.width+'% + '+this.r.left+'%)';
     },
+    */
 
   },
 
