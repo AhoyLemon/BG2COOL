1bc5522a06f0dd8a29251b2b159b19b0e4f831a6
Lemon <3413200+AhoyLemon@users.noreply.github.com>
2019-08-19 17:12:14 -0500

    outta here.

--- js/partials/_vue.js
+++ js/partials/_vue.js
@@ -4,6 +4,13 @@ var app = new Vue({
     phase: 'begin',
     activeTitle: 'none',
     bgPattern: '',
+    bg: {
+      size: 50,
+      fixed: true,
+      //position: 'center'
+      positionHorizontal: 50,
+      positionVertical: 10
+    },
     currentPattern: null
   },
 
@@ -21,6 +28,41 @@ var app = new Vue({
   },
 
   computed: {
+
+    styleObject() {
+      let self = this;
+      let s = {}
+      if (self.bgPattern) {
+        s.backgroundImage = 'url('+self.bgPattern+')';
+      }
+      if (self.bg.size) {
+        s.backgroundSize = self.bg.size+'%';
+      }
+      if (self.bg.positionHorizontal == 50 && self.bg.positionVertical == 50) {
+        s.backgroundPosition = 'center';
+      }
+      else {
+        s.backgroundPosition = self.bg.positionHorizontal+'% '+ self.bg.positionVertical+'%';
+        console.log(s);
+      }
+      return s;
+    },
+
+    coolCSS() {
+      let self = this;
+      let pre = '.bg {\n';
+      if (self.bgPattern) {
+        pre += '  background-image:url('+self.bgPattern+');\n';
+      }
+      if (self.bg.size) {
+        pre += '  background-size: '+self.bg.size+'%;\n';
+      }
+      pre += '}';
+      return pre;
+    },
+
+    
+
     /*
     rockLeft() {
       return 'calc('+this.s.width+'% + '+this.r.left+'%)';
